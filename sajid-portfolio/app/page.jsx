ab175e031ac8d45778165a5bbcde91e3b4b53c40
Sajid <mdsajidali1803@gmail.com>
2024-08-22 01:10:18 +0530

    Add updated file

--- sajid-portfolio/app/page.jsx
+++ sajid-portfolio/app/page.jsx
@@ -26,7 +26,7 @@ const Home = () => {
               <Button
                 variant="outline"
                 size="lg"
-                className="uppercase flex items-center gap-2"
+                className="uppercase flex items-center gap-2 border-2"
               >
                 <span>Download CV</span>
                 <FiDownload className="text-xl"/>
@@ -35,7 +35,7 @@ const Home = () => {
               <div className="mb-8 xl:mb-0">
                 <Socials 
                   containerStyles="flex gap-6"
-                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                  iconStyles="w-9 h-9 border-2 border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
                 />
               </div>
 
